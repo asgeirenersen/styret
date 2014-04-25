383afb00279a80161ae0289f69702f11faafcff5
Asgeir Enersen <asgeir.enersen@pearson.com>
2014-04-25 15:58:44 +0200

    [Linting]

--- saksbehandling/js/model/google/folder/FolderManager.js
+++ saksbehandling/js/model/google/folder/FolderManager.js
@@ -1,27 +1,27 @@
 
 /**
- * @param {Object} gapi Instance of Google API client
- */
+* @param {Object} gapi Instance of Google API client
+*/
 FolderManager = function (gapi) {
-	var _this = this;
-  this.ready = false;
-  this.gapi = gapi;
-	this.gapi.client.load('drive', 'v2', function () {
-    _this.ready = true;
-  });
+    var _this = this;
+    this.ready = false;
+    this.gapi = gapi;
+    this.gapi.client.load('drive', 'v2', function () {
+        _this.ready = true;
+    });
 }
 
 FolderManager.prototype.getById = function (id) {
-  if (!this.ready) {
-    alert("Not so fast!");
-    return;
-  }
-  var request = this.gapi.client.drive.files.get({
+    if (!this.ready) {
+        alert("Not so fast!");
+        return;
+    }
+    var request = this.gapi.client.drive.files.get({
     'fileId': id
-  });
-  request.execute(function(resp) {
-    console.log('Title: ' + resp.title);
-    console.log('Description: ' + resp.description);
-    console.log('MIME type: ' + resp.mimeType);
-  });
+    });
+    request.execute(function(resp) {
+        console.log('Title: ' + resp.title);
+        console.log('Description: ' + resp.description);
+        console.log('MIME type: ' + resp.mimeType);
+    });
 }
