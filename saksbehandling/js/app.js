09008a065c0a876c2449b021304057427f720a75
Asgeir Enersen <asgeir.enersen@pearson.com>
2014-05-22 15:47:44 +0200

    First steps towards a widget.

--- saksbehandling/js/app.js
+++ saksbehandling/js/app.js
@@ -1,29 +1,25 @@
 /* global require, define */
-define(['model/google/folder/FolderManager', 'jquery'], function (FolderManager, $) {
-    var gapi;
+define([
+    'widget/CaseList',
+    'jquery'
+], function (CaseList, $) {
     
-    var App = function (config, localGapi) {
+    var App = function (config, gapi, appContainer) {
+        this.gapi = gapi;
         this.config = config;
-        gapi = localGapi;
+        this.id = 'App_' + new Date().getTime();
+        this.rootElement = $('<div></div>');
+        this.rootElement.attr('id', this.id);
+        appContainer.append(this.rootElement);
     };
     
     App.prototype.start = function () {
-        var folderManager = new FolderManager(gapi),
-            deferred = folderManager.getById(this.config.openCasesFolder);
+        var caseList = new CaseList(this.gapi, this, this.config);
+        caseList.init();
+    };
     
-        deferred.done(function (resp) {
-            var output = $('#output'),
-                dl;
-            output.empty();
-            output.append('<dl></dl>');
-            dl = $('dl', output);
-            dl.append('<dt>Title</dt>');
-            dl.append('<dd>' + resp.title + '</dd>');
-            dl.append('<dt>Description</dt>');
-            dl.append('<dd>' + resp.description + '</dd>');
-            dl.append('<dt>MIME type</dt>');
-            dl.append('<dd>' + resp.mimeType + '</dd>');
-        });
+    App.prototype.getRootElement = function () {
+        return this.rootElement;
     };
     
     return App;
---
+++ saksbehandling/js/widget/CaseList.js
@@ -0,0 +1,58 @@
+
+define(['model/google/folder/FolderManager', 'jquery'], function (FolderManager, $) {
+    var instance = null;
+
+    var CaseList = function (gapi, app, config) {
+        if (instance === null) {
+            instance = this;
+        }
+        this.gapi = gapi;
+        this.parentApp = app;
+        this.config = config;
+        this.id = 'CaseList_' + new Date().getTime();
+        this.folderManager = new FolderManager(gapi);
+    };
+    
+    CaseList.prototype.init = function () {
+        var _this = this;
+        this.rootElement = $('<div></div>');
+        this.rootElement.attr('id', this.id);
+        this.parentApp.getRootElement().append(this.buildUI());
+        $('input[name="getCases"]', this.rootElement).on('click', function (e) {
+            var id = $('input[name="fileId"]', _this.rootElement).val();
+            _this.listCases(id);
+        });
+    };
+    
+    CaseList.prototype.listCases = function (id) {
+        var _this = this,
+            deferred = this.folderManager.getById(this.config.openCasesFolder);
+        
+        deferred.done(function (resp) {
+            var output = $('.listWrapper', _this.rootElement),
+                dl;
+            output.empty();
+            output.append('<dl></dl>');
+            dl = $('dl', output);
+            dl.append('<dt>Title</dt>');
+            dl.append('<dd>' + resp.title + '</dd>');
+            dl.append('<dt>Description</dt>');
+            dl.append('<dd>' + resp.description + '</dd>');
+            dl.append('<dt>MIME type</dt>');
+            dl.append('<dd>' + resp.mimeType + '</dd>');
+        });
+    };
+    
+    CaseList.prototype.buildUI = function () {
+        var html = $('<form>' +
+                '<label value="Case ID">' +
+                '<input name="fileid" type="text">' +
+                '</label>' +
+                '<input name="getCases" type="button" value="Get cases">' +
+                '<div class="listWrapper"></div>' +
+            '</form>');
+        return html;
+    };
+
+    return CaseList;
+});
