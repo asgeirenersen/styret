439fda17645b1b3250748b74dcff0508a5f561f0
Asgeir Enersen <asgeir.enersen@pearson.com>
2014-05-28 14:40:33 +0200

    Handling auth via module.

--- saksbehandling/js/app.js
+++ saksbehandling/js/app.js
@@ -1,27 +1,88 @@
 /* global require, define */
 define([
+    'AuthorizationManager',
     'widget/CaseList',
     'jquery'
-], function (CaseList, $) {
+], function (AuthorizationManager, CaseList, $) {
     
-    var App = function (config, gapi, appContainer) {
+    /**
+     * @param {object} config
+     * @param {gapi} gapi Google api client
+     * @param {string} clientId Google API client id
+     * @param {$} wrapperElement The element where this app will inject itself
+     */
+    var App = function (config, gapi, clientId, wrapperElement) {
         this.widgets = [];
         this.gapi = gapi;
+        this.clientId = clientId;
+        this.scopes = 'https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/drive';
+        this.am = new AuthorizationManager(gapi, clientId);
         this.config = config;
-        this.appContainer = appContainer;
+        this.wrapperElement = wrapperElement;
         this.id = 'App_' + new Date().getTime();
         this.rootElement = this.buildUI();
-        this.appContainer.append(this.rootElement);
+        this.wrapperElement.append(this.rootElement);
     };
     
+    /**
+     * Authorizes and adds the widgets.
+     */
     App.prototype.start = function () {
+        var _this = this,
+            deferred = this.authorize(false);
+        
+        deferred.done(function () {
+            console.log('Auth success!');
+            _this.addWidgets();
+        });
+        deferred.fail(function () {
+            console.log('Auth failed!');
+        });
+    };
+    
+    App.prototype.authorize = function (userInitiated) {
+        var _this = this,
+            result = $.Deferred(),
+            deferred = this.am.authorize(this.scopes, !userInitiated);
+        
+        deferred.done(function (loggedIn) {
+            if (!loggedIn) {
+                _this.showLogin();
+                result.reject(false);
+            } else {
+                _this.hideLogin();
+                result.resolve(true);
+            }
+        });
+        
+        return result;
+    };
+    
+    App.prototype.showLogin = function () {
+        $('.authorize-button', this.rootElement).css('display', 'block');
+    };
+    
+    App.prototype.hideLogin = function () {
+        $('.authorize-button', this.rootElement).css('display', 'none');
+    };
+    
+    /**
+     * Adds all widgets to the menu.
+     */
+    App.prototype.addWidgets = function () {
         var caseList = new CaseList(this.gapi, this, this.config);
         caseList.init();
         
         this.addWidget(caseList, 'Saksliste');
-        this.switchToWidget(caseList);
+        this.switchToWidget(caseList);  
     };
     
+    /**
+     * Adds a widget to the app, and creates a link to it on the menu bar.
+     * 
+     * @param {Object} widget
+     * @param {string} title
+     */
     App.prototype.addWidget = function (widget, title) {
         var _this = this,
             link = $('<a href="#">' + title + '</a>'),
@@ -36,6 +97,11 @@ define([
         $('ul.nav', this.rootElement).append(item);
     };
     
+    /**
+     * Displays the specified widget and closes the currently active one.
+     * 
+     * @param {Object} widget
+     */
     App.prototype.switchToWidget = function (widget) {
         var i = 0;
         for (i; i < this.widgets.length; i = i + 1) {
@@ -46,6 +112,11 @@ define([
         widget.show();
     };
     
+    /**
+     * Builds the markup for the application.
+     * 
+     * @returns {$} The root element containing the DOM fragment for the app.
+     */
     App.prototype.buildUI = function () {
         var menuBar,
             rootElement;
@@ -55,10 +126,22 @@ define([
         menuBar = $('<div class="navbar navbar-default"></div>');
         menuBar.append('<div class="navbar-collapse collapse"><ul class="nav navbar-nav"></ul></div>');
         rootElement.append(menuBar);
+        rootElement.append('<button type="button" class="btn btn-danger authorize-button" style="visibility: hidden">Logg inn</button>');
         
         return rootElement;
-    }
+    };
     
+    App.prototype.addListeners = function () {
+        var _this = this;
+
+        $('authorize-button', this.rootElement).on('click', null, function () {
+            _this.authorize(true);
+        });
+    };
+    
+    /**
+     * @returns {$} The root element for the app.
+     */
     App.prototype.getRootElement = function () {
         return this.rootElement;
     };
