f339fcf9f6da7eef2d82f713aecc89b9c116c9e5
Asgeir Enersen <asgeir@enersen.com>
2014-06-01 12:32:22 +0200

    Refactored the case listing.

--- saksbehandling/js/widget/CaseList.js
+++ saksbehandling/js/widget/CaseList.js
@@ -39,98 +39,123 @@ define(['model/google/folder/FolderManager', 'jquery'], function (FolderManager,
     }
     
     CaseList.prototype.listOpenCases = function () {
-        this.listCases(this.config.openCasesFolder);
+        var _this = this,
+            listReady = this.listCases(this.config.openCasesFolder);
+        
+        listReady.done(function () {
+            $('.rowButtons', _this.rootElement).each(function () {
+                $(this).append('<a class="closeBtn" href="#">&nbsp;(Lukk)</a>');
+            });
+
+            $('.closeBtn', _this.rootElement).on('click', null, function (e) {
+                var clickedElement = $(e.target),
+                    tr = clickedElement.closest('tr'),
+                    id = tr.attr('data-id'),
+                    title = $('.caseTitle', tr).text(),
+                    confirmTxt = 'Vil du lukke denne saken?\n' + title;
+
+                e.preventDefault();
+                if (confirm(confirmTxt)) {
+                    _this.moveCase(id, _this.config.openCasesFolder, _this.config.closedCasesFolder, function () {
+                        this.listOpenCases();
+                    });
+                }
+            });
+        });
     };
     
     CaseList.prototype.listClosedCases = function () {
-        this.listCases(this.config.closedCasesFolder);
+        var _this = this,
+            listReady = this.listCases(this.config.closedCasesFolder);
+        
+        listReady.done(function () {
+            $('.rowButtons', _this.rootElement).each(function () {
+                $(this).append('<a class="reopenBtn" href="#">&nbsp;(Gjenåpne)</a>');
+            });
+
+            $('.reopenBtn', _this.rootElement).on('click', null, function (e) {
+                var clickedElement = $(e.target),
+                    tr = clickedElement.closest('tr'),
+                    id = tr.attr('data-id'),
+                    title = $('.caseTitle', tr).text(),
+                    confirmTxt = 'Vil du gjenåpne denne saken?\n' + title;
+
+                e.preventDefault();
+                if (confirm(confirmTxt)) {
+                    _this.moveCase(id, _this.config.closedCasesFolder, _this.config.openCasesFolder, function () {
+                        this.listClosedCases();
+                    });
+                }
+            });
+        });
     };
     
     CaseList.prototype.listPossibleCases = function () {
-        this.listCases(this.config.possibleCasesFolder);
+        var _this = this,
+            listReady = this.listCases(this.config.possibleCasesFolder);
+        
+        listReady.done(function () {
+            $('.rowButtons', _this.rootElement).each(function () {
+                $(this).append('<a class="openBtn" href="#">&nbsp;(Åpne)</a>');
+            });
+
+            $('.openBtn', _this.rootElement).on('click', null, function (e) {
+                var clickedElement = $(e.target),
+                    tr = clickedElement.closest('tr'),
+                    id = tr.attr('data-id'),
+                    title = $('.caseTitle', tr).text(),
+                    confirmTxt = 'Vil du åpne denne saken?\n' + title;
+
+                e.preventDefault();
+                if (confirm(confirmTxt)) {
+                    _this.moveCase(id, _this.config.possibleCasesFolder, _this.config.openCasesFolder, function () {
+                        this.listPossibleCases();
+                    });
+                }
+            });
+        });
     };
     
     CaseList.prototype.listCases = function (parentFolderId) {
         var _this = this,
+            retVal = $.Deferred(),
             deferred = this.folderManager.getFoldersByParentId(parentFolderId);
         
         deferred.done(function (resp) {
             var output = $('.listWrapper', _this.rootElement),
                 items = resp['items'],
-                list = $('<ul></ul>');
+                table = $('<table></table>'),
+                i = 0;
             output.empty();
-            for (var i = 0; i < items.length; i = i +1) {
-                var closeLink = $('<a class="closeBtn" data-id="' + items[i]['id'] + '" href="#">&nbsp;(Lukk)</a>'),
-                    li = $('<li></li>');
-
-                li.append('<span class="caseTitle">' + items[i]['title'] + '</span>');
-                li.append(closeLink);
-                list.append(li);
+            for (i; i < items.length; i = i +1) {
+                var tr = $('<tr></tr>'),
+                    buttonTd = $('<td class="rowButtons"></td>');
+                tr.attr('data-id', items[i]['id']);
+                tr.append('<td class="caseTitle">' + items[i]['title'] + '</td>');
+                tr.append(buttonTd);
+                table.append(tr);
             }
-            output.append(list);
-            $('.closeBtn', output).on('click', null, function (e) {
-                var clickedElement = $(e.target),
-                    id = clickedElement.attr('data-id'),
-                    title = clickedElement.prev('span').text(),
-                    confirmTxt = 'Vil du lukke denne saken?\n' + title;
-
-                e.preventDefault();
-                if (confirm(confirmTxt)) {
-                    var whenAdded,
-                        whenRemoved,
-                        whenAll = $.Deferred(),
-                        reportDone;
-                        
-                    reportDone = function () {
-                        if (whenAdded.state() === 'resolved' && 
-                                whenRemoved.state() === 'resolved') {
-                            whenAll.resolve();
-                        }
-                    }
-                        
-                    whenAll.done(function () {
-                        _this.listCases(parentFolderId);
-                    });
-
-                    whenAdded = _this.folderManager.addParent(id, _this.config.closedCasesFolder);
-                    whenAdded.done(function (resp) {
-                        if (resp['error']) {
-                            alert('Kunne ikke legge saken inn i mappen 02 Lukkede saker.');
-                        } else {
-                            console.log('Saken er lagt til i mappen 02 Lukkede saker.');
-                        }
-                        reportDone(whenAdded);
-                    });
-                    whenRemoved = _this.folderManager.removeParent(id, _this.config.openCasesFolder);
-                    whenRemoved.done(function (resp) {
-                        if (resp['error']) {
-                            alert('Saken kunne ikke fjernes fra mappen 01 Åpne saker.');
-                        } else {
-                            console.log('Saken er fjernet fra mappen 01 Åpne saker.');
-                        }
-                        reportDone(whenRemoved);
-                    });
-                    
-                }
-
-            });
+            output.append(table);
+            retVal.resolve();
         });
+        
+        return retVal;
     };
     
-    CaseList.prototype.closeCase = function (id) {
+    CaseList.prototype.moveCase = function (id, fromFolderId, toFolderId, callback) {
         var _this = this,
             deferred1,
             deferred2;
             
-        deferred1 = this.folderManager.addParent(id, this.config.closedCasesFolder);
+        deferred1 = this.folderManager.addParent(id, toFolderId);
         deferred1.done(function () {
-            deferred2 = _this.folderManager.removeParent(id, _this.config.openCasesFolder);
+            deferred2 = _this.folderManager.removeParent(id, fromFolderId);
             deferred2.done(function (resp) {
                 console.debug(resp);
-                _this.listCases();
+                callback.call(_this);
             });
         });
-    }
+    };
     
     CaseList.prototype.buildUI = function () {
         var rootElement = $('<div></div>'),
