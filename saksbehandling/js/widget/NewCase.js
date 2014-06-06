426947a96ee5638a080d5419e87fd76ab6fa49dc
Asgeir Enersen <asgeir.enersen@pearson.com>
2014-06-06 13:45:50 +0200

    Added styling to NewCase form, and implemented status selector.

--- saksbehandling/js/widget/NewCase.js
+++ saksbehandling/js/widget/NewCase.js
@@ -1,4 +1,11 @@
-
+/**
+ * A widget for creating a new case.
+ *
+ * @param {type} FolderManager
+ * @param {type} CaseManager
+ * @param {type} $
+ * @returns {_L8.NewCase}
+ */
 define([
     'model/google/folder/FolderManager',
     'model/core/case/CaseManager',
@@ -43,7 +50,7 @@ define([
         deferred = this.caseManager.createCase(
                 $('input[name="title"]', this.rootElement).val(),
                 $('input[name="description"]', this.rootElement).val(),
-                this.caseManager.statusOpen
+                $('input[name="status"]:checked', this.rootElement).val()
         );
 
         return deferred;
@@ -56,15 +63,22 @@ define([
     NewCase.prototype.buildUI = function () {
         var rootElement = $('<div></div>'),
             head = $('<h2></h2>').text('Opprett ny sak'),
-            form = $('<form name="newCase" class="newCase"></form>'),
+            form = $('<form name="newCase" role="form"></form>'),
             titleLabel = $('<label>Tittel</label>'),
-            titleInput = $('<input type="text" name="title">'),
+            titleInput = $('<input type="text" name="title" class="form-control">'),
             descriptionLabel = $('<label>Beskrivelse</label>'),
-            descriptionInput = $('<textarea name="description" rows="5" cols="30"></textarea>'),
-            btn = $('<button type="button" class="btn btn-sm btn-primary">Lagre</button>');
+            descriptionInput = $('<textarea name="description" rows="5" cols="30" class="form-control"></textarea>'),
+            btn = $('<div><button type="button" class="btn btn-sm btn-primary">Lagre</button></div>'),
+            radioOpen = $('<label class="radio-inline"><input type="radio" name="status" value="' + this.caseManager.statusOpen + '" checked> Åpen</label>'),
+            radioPossible = $('<label class="radio-inline"><input type="radio" name="status" value="' + this.caseManager.statusPossible + '"> Kanskje</label>'),
+            radioClosed = $('<label class="radio-inline"><input type="radio" name="status" value="' + this.caseManager.statusClosed + '"> Lukket</label>'),
+            formGroupStr = '<div class="form-group"></div>';
     
-        form.append(titleLabel.append(titleInput));
-        form.append(descriptionLabel.append(descriptionInput));
+        form.append($(formGroupStr).append(titleLabel.append(titleInput)));
+        form.append($(formGroupStr).append(descriptionLabel.append(descriptionInput)));
+        form.append($(radioOpen));
+        form.append($(radioClosed));
+        form.append($(radioPossible));
         form.append(btn);
         
         rootElement.append(head);
